81ca55788bce9834655c7414f8942ab707da639a
Ushio Hiroto <g1644132.cac@gmail.com>
2017-08-18 03:28:25 +0900

    MainManagerの制作

---
+++ MainApp/src/MainApp/CsvProcess.java
@@ -0,0 +1,25 @@
+package MainApp;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import java.nio.file.attribute.FileStoreAttributeView;
+
+/**
+ *
+ * @author ushiohiroto
+ */
+public class CsvProcess {
+    File file;
+    
+    void readCSV() throws FileNotFoundException{
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        
+        
+        
+    }
+}
---
+++ MainApp/src/MainApp/FXMLDocumentController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainApp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import java.io.*;
+
+/**
+ *
+ * @author ushiohiroto
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void QuitAction(ActionEvent event) {
+        System.out.println("You Selected Quit from menu.");
+        Platform.exit();
+    }
+    
+    @FXML
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
