8c8c009bb18a52ddb7bca7c9e29742d8ca2cc6b2
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2017-08-19 01:34:38 +0900

    クイックソートの実装,動作確認はしてない

--- system/src/koyamaFesAppJudge/AnswerManager.java
+++ system/src/koyamaFesAppJudge/AnswerManager.java
@@ -47,7 +47,40 @@ public class AnswerManager {
     private void sortAnswerData(){
         if(TRACE) System.out.println("sortAnswerData : 開発中");
 
+        this.quickSort(answerData);
         //answerData.sort(Comparator.comparing(Answer::getSt)); クイックじゃないしこんなコンパレータでちゃんと動くかわかんないので殴り書きしておく
+    }
+
+
+    /**
+     * quickSort
+     * @param list ソートしたいリスト
+     * @return ソートされたリスト
+     */
+    private ArrayList<Answer> quickSort(ArrayList<Answer> list){
+        if(list.size() < 2) return list;
+
+        ArrayList<Answer> smaller = new ArrayList<>();
+        ArrayList<Answer> bigger = new ArrayList<>();
+        ArrayList<Answer> result = new ArrayList<>();
+
+
+        //軸の設定　else使いたくなかった・・・
+        Long key;
+        if(list.get(0).getSt().getTimeInMillis() > list.get(1).getSt().getTimeInMillis()) key = list.get(0).getSt().getTimeInMillis();
+        else key = list.get(1).getSt().getTimeInMillis();
+
+
+        //軸よりデカいかで振り分けます アルゴリの教科書では要素をスワップしてたけどコレクション大量生成しちゃうクソプログラムなのでこれでもよいかと
+        for(Answer ans : list) {
+            if(ans.getSt().getTimeInMillis() >= key) bigger.add(ans);
+            else smaller.add(ans);
+        }
+
+
+        result.addAll(quickSort(smaller));
+        result.addAll(quickSort(bigger));
+        return result;
     }
 
 }
