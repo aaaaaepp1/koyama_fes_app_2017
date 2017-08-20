1b78bc3e8e9811ebaffcb4daf502275fa7f7e780
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2017-08-21 01:40:25 +0900

    HttpURLConnectionにしつつちょこちょこいじった、SetUriとか

--- system/src/koyamaFesAppJudge/NetworkControlManager.java
+++ system/src/koyamaFesAppJudge/NetworkControlManager.java
@@ -3,44 +3,48 @@ package koyamaFesAppJudge;
 import com.sun.deploy.trace.Trace;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class NetworkControlManager {
 
     private final boolean DEBUG = false;
     private final boolean TRACE = true;
 
-    private URLConnection urlConnection;
-    private ArrayList<String[]> list;
+    private HttpURLConnection urlConnection;
 
     //コンストラクタ
-    public NetworkControlManager(String uri) {
+    public NetworkControlManager(String uri) throws IOException{
         if(TRACE) System.out.println("NetworkControlManager : 開発中");
 
         this.setUri(uri);
     }
 
 
-    public Boolean setUri(String uri) {
+    public Boolean setUri(String uri) throws IOException{
         if(TRACE) System.out.println("setUri : 開発中");
 
         try {
-            urlConnection = new URL(uri).openConnection();
-        } catch(Exception e) {
+            urlConnection = (HttpURLConnection) new URL(uri).openConnection();
+        } catch(MalformedURLException e) { //new URL()で発生しうる例外 有効なプロトコルが見つからないor文字列を解析できない
             e.printStackTrace();
             return false;
         }
-        return true;
+
+        //応答メッセージから状態コードを取得して200番だったら
+        if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            return true;
+        }
+        return false;
     }
 
 
     public AnswerManager getAnswer() throws IOException{
         if(TRACE) System.out.println("getAnswer : 開発中");
 
-        list = new ArrayList<>();
+        ArrayList<String[]> list = new ArrayList<>();
         AnswerManager answerManager = new AnswerManager(list);
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -50,7 +54,7 @@ public class NetworkControlManager {
             e.printStackTrace();
         }
 
-        //AnswerManagerのインスタンスのanswerDataを更新できてないのでダメ
+        //AnswerManagerのインスタンスのanswerDataを更新できてないけどきっとこれを使う人が.getAnswerData()(=ソート)してくれるだろう
         return answerManager;
     }
 }
